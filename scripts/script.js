7c4801a6d030946b63c98576d04de53cd1fcfd28
BohdanMoyseyets <y.stetsko@bistox.com>
2022-07-21 21:42:34 +0300

    desctop ready

--- scripts/script.js
+++ scripts/script.js
@@ -63,5 +63,9 @@ $(document).ready(function () {
         },
     });
     $(".slider-content").show();
+    $("#go_top").click(function(){
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
 
 });
